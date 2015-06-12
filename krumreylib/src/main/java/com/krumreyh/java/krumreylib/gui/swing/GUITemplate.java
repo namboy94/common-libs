7848da02e4f8de3e4cc43d234f33f07e924b8df0
hermann <hermann@krumreyh.com>
2015-06-12 10:17:14 +0200

    Added GUITemplate and a JUnit Test class for it.

---
+++ krumreylib/src/main/java/com/krumreyh/java/krumreylib/gui/swing/GUITemplate.java
@@ -0,0 +1,25 @@
+package com.krumreyh.java.krumreylib.gui.swing;
+
+/**
+ * Class that extends Swing's JFrame and has methods that simplify the adding of
+ * GUI objects.
+ * @author Hermann Krumrey
+ * @version 0.1
+ */
+public class GUITemplate {
+	
+	/**
+	 * Main method that starts the GUI
+	 * @param args - command line parameters
+	 */
+	public static void main(String[] args) {
+		new GUITemplate();
+	}
+	
+	/**
+	 * Constructor of the GUI
+	 */
+	public GUITemplate() {
+	}
+	
+}
---
+++ krumreylib/src/test/java/com/krumreyh/java/krumreylib/gui/swing/GUITemplateTest.java
@@ -0,0 +1,10 @@
+package com.krumreyh.java.krumreylib.gui.swing;
+
+/**
+ * JUnit Class that test GUITemplate.java
+ * @author Hermann Krumrey
+ * @version 0.1
+ */
+public class GUITemplateTest {
+
+}
