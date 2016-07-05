0678bed35518c12f3f0e6f8747d90d71765f51f3
Hermann Krumrey <hermann@krumreyh.com>
2016-07-05 12:28:07 +0200

    Added metadata

--- puffotter/metadata.py
+++ puffotter/metadata.py
@@ -21,3 +21,99 @@ This file is part of puffotter.
     along with puffotter.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE
 """
+
+"""
+The metadata is stored here. It can be used by any other module in this project this way, most
+notably by the setup.py file
+"""
+
+project_name = "puffotter"
+"""
+The name of the project
+"""
+
+project_description = "A python library to de-clutter other projects"
+"""
+A short description of the project
+"""
+
+version_number = "0.0.1"
+"""
+The current version of the program.
+"""
+
+development_status = "Development Status :: 1 - Planning"
+"""
+The current development status of the program
+"""
+
+project_url = "http://namibsun.net/namboy94/puffotter"
+"""
+A URL linking to the home page of the project, in this case a
+self-hosted Gitlab page
+"""
+
+download_url = "http://gitlab.namibsun.net/namboy94/puffotter/repository/archive.zip?ref=master"
+"""
+A URL linking to the current source zip file.
+"""
+
+author_name = "Hermann Krumrey"
+"""
+The name(s) of the project author(s)
+"""
+
+author_email = "hermann@krumreyh.com"
+"""
+The email address(es) of the project author(s)
+"""
+
+license_type = "GNU GPL3"
+"""
+The project's license type
+"""
+
+python3_requirements = []
+"""
+Python 3 Python Packaging Index requirements
+"""
+
+python2_requirements = []
+"""
+Python 2 Python Packaging Index requirements
+"""
+
+audience = "Intended Audience :: Developers"
+"""
+The intended audience of this software
+"""
+
+environment = "Environment :: Other Environment"
+"""
+The intended environment in which the program will be used
+"""
+
+programming_language = 'Programming Language :: Python :: 3'
+"""
+The programming language used in this project
+"""
+
+topic = "Topic :: Utilities"
+"""
+The broad subject/topic of the project
+"""
+
+language = "Natural Language :: English"
+"""
+The (default) language of this project
+"""
+
+compatible_os = "Operating System :: OS Independent"
+"""
+The Operating Systems on which the program can run
+"""
+
+license_identifier = "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+"""
+The license used for this project
+"""
